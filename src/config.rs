bc93cbeb0e857b85cf735b6a0dc36a058f566738
JF Pambrun <jf.pambrun@gmail.com>
2026-02-21 10:45:12 -0500

    Switch default desktop shortcuts to LWIN

--- src/config.rs
+++ src/config.rs
@@ -11,14 +11,15 @@ use std::io::Write;
 pub fn get_default() -> JsonValue {
 	object! {
 		shortcuts: {
-			desktop_1: "LALT+LSHIFT+1",
-			desktop_2: "LALT+LSHIFT+2",
-			desktop_3: "LALT+LSHIFT+3",
-			desktop_4: "LALT+LSHIFT+4",
-			desktop_5: "LALT+LSHIFT+5",
-			desktop_6: "LALT+LSHIFT+6",
-			desktop_7: "LALT+LSHIFT+7",
-			desktop_8: "LALT+LSHIFT+8",
+			desktop_1: "LWIN+1",
+			desktop_2: "LWIN+2",
+			desktop_3: "LWIN+3",
+			desktop_4: "LWIN+4",
+			desktop_5: "LWIN+5",
+			desktop_6: "LWIN+6",
+			desktop_7: "LWIN+7",
+			desktop_8: "LWIN+8",
+			desktop_9: "LWIN+9",
 		}
 	}
 }
